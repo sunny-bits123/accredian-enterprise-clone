01b0c98549583780a91e0a798741ee4da43ec804
sunny-bits123 <sunny9695457219@gmail.com>
2026-04-24 18:12:43 +0530

    Create main page

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import Benefits from "@/components/Benefits";
+import Pricing from "@/components/Pricing";
+import LeadForm from "@/components/LeadForm";
+import CTA from "@/components/CTA";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Benefits />
+      <Pricing />
+      <CTA />
+      <LeadForm />
+    </>
+  );
+}
